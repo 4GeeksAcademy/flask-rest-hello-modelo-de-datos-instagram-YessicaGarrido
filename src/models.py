42ede632eb2aeb2776cb1830c85d1e92adb00bf8
yessigarridowork-blip <yessigarrido.work@gmail.com>
2025-12-11 21:43:23 +0000

    mi diagrama

--- src/models.py
+++ src/models.py
@@ -1,14 +1,43 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(nullable=False)
+    lastname: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+
+
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped["User"] = relationship()
+    user_to_id: Mapped["User"] = relationship()
+
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[int] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    post_id: Mapped["Post"] = relationship()
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped["User"] = relationship()
+    
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column( unique=True, nullable=False)
+    author_id: Mapped["User"] = relationship()
+    post_id: Mapped["User"] = relationship()
+    
+          
 
 
     def serialize(self):
